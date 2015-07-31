907a21c72460c588d2b41c05c98746beb8d9acec
Matheus Martins <matheusmsvianna@gmail.com>
2015-07-31 11:03:48 -0300

    casos de uso e create_tables atualizados

--- create_tables.sql
+++ create_tables.sql
@@ -1,50 +1,49 @@
 CREATE TABLE tipo_usuario (
-  id_tipo_usuario INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_tipo_usuario INTEGER UNSIGNED  AUTO_INCREMENT,
   tipo_usuario VARCHAR(64) NOT NULL,
   PRIMARY KEY(id_tipo_usuario)
 );
 
 CREATE TABLE status_usuario (
-  id_status_usuario INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_status_usuario INTEGER UNSIGNED  AUTO_INCREMENT,
   status_usuario VARCHAR(64) NOT NULL,
   PRIMARY KEY(id_status_usuario)
 );
 
 CREATE TABLE usuario (
-  id_usuario INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_usuario INTEGER UNSIGNED  AUTO_INCREMENT,
   login VARCHAR(64) NOT NULL,
   senha VARCHAR(64) NOT NULL,
-  fk_tipo_usuario INTEGER UNSIGNED NOT NULL,
   fk_status_usuario INTEGER UNSIGNED NOT NULL,
   PRIMARY KEY(id_usuario),
-  CONSTRAINT fk_tipo_usuario
-	  FOREIGN KEY(fk_tipo_usuario)
-		REFERENCES tipo_usuario(id_tipo_usuario)
-		ON UPDATE CASCADE,
   CONSTRAINT fk_status_usuario
 	  FOREIGN KEY(fk_status_usuario)
 		REFERENCES status_usuario(id_status_usuario)
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE medico (
-  id_medico INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE funcionario (
+  fk_tipo_funcionario INTEGER UNSIGNED NOT NULL,
   fk_usuario INTEGER UNSIGNED NOT NULL,
   nome VARCHAR(64) NOT NULL,
   cpf CHAR(11) NOT NULL,
-  PRIMARY KEY(id_medico),
+  endereço VARCHAR(128) NOT NULL,
+  PRIMARY KEY(fk_usuario),
   CONSTRAINT fk_usuario_medico
 	  FOREIGN KEY(fk_usuario)
 		REFERENCES usuario(id_usuario)
 		ON UPDATE CASCADE
+  CONSTRAINT fk_tipo_funcionario
+	  FOREIGN KEY(fk_tipo_usuario)
+		REFERENCES tipo_usuario(id_tipo_usuario)
+		ON UPDATE CASCADE,
 );
 
 CREATE TABLE ponto (
-  id_ponto INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_ponto INTEGER UNSIGNED  AUTO_INCREMENT,
   fk_usuario INTEGER UNSIGNED NOT NULL,
-  hora_entrada TIME NOT NULL,
-  hora_saida TIME ,
-  data_ DATE NOT NULL,
+  dh_entrada DATETIME NOT NULL,
+  dh_saida DATETIME ,
   horas_trabalhadas INTEGER UNSIGNED,
   PRIMARY KEY(id_ponto),
   CONSTRAINT fk_usuario_ponto
@@ -54,26 +53,26 @@ CREATE TABLE ponto (
 );
 
 CREATE TABLE paciente (
-  id_paciente INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_paciente INTEGER UNSIGNED  AUTO_INCREMENT,
   nome VARCHAR(64) NOT NULL,
   cpf CHAR(11) NOT NULL,
   PRIMARY KEY(id_paciente)
 );
 
 CREATE TABLE tipo_quarto (
-  id_tipo_quarto INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_tipo_quarto INTEGER UNSIGNED  AUTO_INCREMENT,
   tipo_quarto VARCHAR(64) NOT NULL,
   PRIMARY KEY(id_tipo_quarto)
 );
 
 CREATE TABLE status_quarto (
-  id_status_quarto INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_status_quarto INTEGER UNSIGNED  AUTO_INCREMENT,
   status_quarto VARCHAR(64) NOT NULL,
   PRIMARY KEY(id_status_quarto)
 );
 
 CREATE TABLE quarto (
-  id_quarto INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_quarto INTEGER UNSIGNED  AUTO_INCREMENT,
   fk_tipo_quarto INTEGER UNSIGNED NOT NULL,
   fk_status_quarto INTEGER UNSIGNED NOT NULL,
   quarto INTEGER UNSIGNED NOT NULL,
@@ -87,13 +86,13 @@ CREATE TABLE quarto (
 );
 
 CREATE TABLE status_saude (
-  id_status_saude INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_status_saude INTEGER UNSIGNED  AUTO_INCREMENT,
   status_saude VARCHAR(64) NOT NULL,
   PRIMARY KEY(id_status_saude)
 );
 
 CREATE TABLE prontuario (
-  id_prontuario INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_prontuario INTEGER UNSIGNED  AUTO_INCREMENT,
   fk_quarto INTEGER UNSIGNED NOT NULL,
   fk_medico INTEGER UNSIGNED NOT NULL,
   fk_paciente INTEGER UNSIGNED NOT NULL,
